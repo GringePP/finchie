c88ccde4e442509ebd04a7eea1ee79cc5f9bc939
Mindjet <pearl920@outlook.com>
2019-07-02 22:16:25 +0800

    [+]: add pull-to-refresh

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:finchie/network/yuque_api.dart';
 import 'package:finchie/page/introduce_page.dart';
 import 'package:finchie/widget/card_generator.dart';
+import 'package:finchie/widget/repo_widgets.dart' as repoWidgets;
 import 'package:finchie/widget/user_info_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:finchie/page/info_page.dart';
@@ -33,7 +34,6 @@ class _HomePageState extends ScaffoldState {
 
   @override
   void initState() {
-    // fetch(USER_INFO_URL, onSuccess: _onGetUserInfo);
     fetch(USER_REPOS_URL, onSuccess: _onGetRepos);
   }
 
@@ -43,114 +43,12 @@ class _HomePageState extends ScaffoldState {
     });
   }
 
-  _onGetUserInfo(data) {
-    setState(() {
-      userInfo = data;
-    });
-    final userId = data["id"];
-    final url = '$BASE_URL/users/$userId/groups';
-    fetch(url, onSuccess: _onGetGroups);
-  }
-
-  _onGetGroups(data) {
-    setState(() {
-      groups = data;
-    });
+  Future<Null> _onRefresh() async {
+    await fetch(USER_REPOS_URL, onSuccess: _onGetRepos);
   }
 
   @override
   Widget build(BuildContext context) {
-    Container buildGroups(groups) {
-      bool groupsNull = groups == null;
-      if (groupsNull) return buildCardContainer(Container());
-      List<Widget> listItems = [];
-      for (var group in groups) {
-        listItems.add(ListTile(
-          title: Text(
-            group["name"],
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          subtitle: Text(
-            group["description"] ?? "No description",
-          ),
-          leading: Container(
-            width: 30,
-            height: 30,
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(group["avatar_url"]),
-              backgroundColor: Colors.grey[500],
-            ),
-          ),
-        ));
-      }
-      return buildCardContainer(
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: listItems,
-          ),
-          padding: EdgeInsets.all(5));
-    }
-
-    Container buildTitle(String label) {
-      return Container(
-        margin: EdgeInsets.only(left: 10, top: 10),
-        child: Text(
-          label,
-          style: TextStyle(
-              color: Colors.accents[0],
-              fontWeight: FontWeight.w800,
-              fontSize: 22),
-          textAlign: TextAlign.start,
-        ),
-      );
-    }
-
-    Widget buildRepos(repos) {
-      if (repos == null) {
-        Widget item = Center(
-          child: Text(
-            '知识库为空',
-            style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-                color: Colors.grey[500]),
-          ),
-        );
-        return item;
-      }
-      List<Widget> items = [];
-      for (var repo in repos) {
-        items.add(Card(
-          elevation: 2.5,
-          child: ListTile(
-            leading: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                Text(
-                  repo["name"][0],
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                )
-              ],
-            ),
-            title: Text(
-              repo["name"],
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(repo["description"]),
-          ),
-        ));
-      }
-      return ListView(
-        children: items,
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.menu),
@@ -159,7 +57,12 @@ class _HomePageState extends ScaffoldState {
               fontWeight: FontWeight.w600,
             )),
       ),
-      body: buildRepos(repos),
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: repos == null
+            ? repoWidgets.emptyRepo
+            : repoWidgets.buildRepoList(context, repos),
+      ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
---
+++ lib/widget/repo_widgets.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+final emptyRepo = Center(
+  child: Text(
+    '知识库为空',
+    style: TextStyle(
+        fontSize: 25, fontWeight: FontWeight.w600, color: Colors.grey[500]),
+  ),
+);
+
+buildRepoList(BuildContext context, dynamic repos) {
+  return ListView.builder(
+      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+      itemCount: repos.length,
+      itemBuilder: (ctx, idx) => Card(
+          elevation: 2.5,
+          child: GestureDetector(
+            onTap: () => {},
+            onLongPress: () => {},
+            child: ListTile(
+              leading: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  Text(
+                    repos[idx]["name"][0],
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  )
+                ],
+              ),
+              title: Text(
+                repos[idx]["name"],
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                repos[idx]["description"],
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ))).build(context);
+}
